c2f67cd264d339a3d6a01476bbf5826b7d7c4b98
Felipe Presas <felipe.presas@gmail.com>
2021-10-28 00:13:34 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList();
+
+
+// todoList.todos.forEach(todo => crearTodoHtml(todo));
+todoList.todos.forEach(crearTodoHtml); // funciona porque es un solo argumento
+
+
+
